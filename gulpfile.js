6aecf2289847b8eb97a18681ceb6899ec1aaec26
krell1983 <piotr.markiewicz.1983@gmail.com>
2016-08-28 16:02:55 +0200

    no message

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp             = require('gulp');
+var browser_sync     = require('browser-sync');    // przetwarzanie plików
+var gulp_plumber     = require('gulp-plumber');    // de bug
+var gulp_uglify      = require('gulp-uglify');     // min files
+var gulp_rename      = require('gulp-rename');     // add min at end
+var gulp_concat      = require('gulp-concat');     // lonczenie plików
+var gulp_sass        = require('gulp-sass');       // Sass
+var gulp_cssmin      = require('gulp-cssmin');     // css-min
+
+var gulp_react       = require('gulp-react');      // JSX
+var gulp_babel       = require('gulp-babel');      // ES-2015
+var gulp_typescript  = require('gulp-typescript'); // typescript
+
+
+
+gulp.task('typescript', function() {
+  gulp.src(['sorce/typescript/*.ts'])
+        .pipe(gulp_typescript({
+             noImplicitAny: true,
+             out: 'output.js'
+         }))
+        .pipe(gulp_concat('typescript.js')) // for testing
+        .pipe(gulp.dest('project/scripts'))
+        .pipe(gulp_uglify())
+        .pipe(gulp_rename({suffix: '.min'}))
+        .pipe(gulp.dest('project/scripts'));
+});
+
+
+gulp.task('watch_typescript', function(){
+gulp.watch(['sorce/typescript/*.ts'],['typescript']);
+});
+
+gulp.task('Test', function(){
+  console.log('Gulp Works');
+});
+
+gulp.task('default', ['watch_typescript']);
---
+++ sorce/typescript/typescripts.ts
@@ -0,0 +1,30 @@
+
+var flag:boolean;
+flag = true;
+flag = false;
+
+var my_number:number = 100;
+
+var my_string:string = "100";
+
+var my_Array:number [] = [1,2,3,4];
+
+var my_Array2:any [] = [1,2,3,4,'Dupa'];
+
+
+
+
+var add3 = ( j:number , h:number):number => {
+  return j + h;
+}
+
+console.log('add3:'+add3(6,77));
+
+function add2( a:number , b:number):number{
+  return a + b;
+}
+
+console.log('add2:'+add2(63,123));
+
+//var MakePoint = () => 1;
+//var MakePoint = function () { return 1; };
